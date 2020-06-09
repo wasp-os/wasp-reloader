397e49dfca18088008b38032ed66cfcad89adef5
Fuji Pebri <pebri86@gmail.com>
2020-06-09 15:42:38 +0700

    add support for p8-watch
    Signed-off-by: Fuji Pebri <pebri86@gmail.com>

---
+++ src/boards/p8/board.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ * Copyright (C) 2020 Daniel Thompson
+ */
+
+#ifndef RELOADER_BOARD_P8_H__
+#define RELOADER_BOARD_P8_H__
+
+#define CONFIG_ST7789
+
+// WDT config
+#define CONFIG_HAVE_WDT_BUTTON   (1)
+#define CONFIG_WDT_BUTTON        (17)
+#define CONFIG_WDT_BUTTON_ACTIVE (1) // active high
+#define CONFIG_WDT_BUTTON_PULL   (0)  // no pull up/down
+
+#endif /* RELOADER_BOARD_P8_H__ */
