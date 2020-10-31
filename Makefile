wasp-os/wasp-reloader

commit 5bf3b20a248f62c7ba535d6990c7c23787f866eb

Add mcuboot support
Originally wasp-reloader was intended to allow systems based on
wasp-bootloader to upgrade to the latest verison. With these changes it
can also be used to replace the factory default mcuboot with the
wasp-bootloader.

Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

Makefile
@@ -16,7 +16,7 @@ CFLAGS += \
 	  -Ilib/nrfx/mdk \
 	  -Ilib/nrfx/hal \
 	  -Isrc/boards/$(BOARD) -Isrc
-LDFLAGS += $(CFLAGS) -Wl,-Map=$(OBJDIR)/reloader.map -Lsrc -Llib/nrfx/mdk -Tnrf52832_xxaa.ld
+LDFLAGS += $(CFLAGS) -Wl,-Map=$(OBJDIR)/reloader.map -Lsrc -Llib/nrfx/mdk -T$(LDSCRIPT)
 
 APP = $(OBJDIR)/reloader.zip
 OBJS = main.o \
@@ -29,9 +29,8 @@ OBJS = main.o \
 OBJDIR = build-$(BOARD)
 OBJS := $(OBJS:%.o=$(OBJDIR)/%.o)
 
-
 all: $(APP)
-
+$(OBJDIR)/reloader.elf: LDSCRIPT = nrf52832_xxaa.ld
 $(OBJDIR)/reloader.elf: $(OBJDIR) $(OBJS) $(LDFILE)
 	$(CROSS_COMPILE)gcc $(LDFLAGS) $(OBJS) -o $@
 
@@ -39,6 +38,20 @@ $(OBJDIR)/reloader.hex: $(OBJDIR)/reloader.elf
 	$(CROSS_COMPILE)objcopy -O ihex $< $@
 
 $(OBJDIR)/reloader.zip : $(OBJDIR)/reloader.hex
+	python3 -m nordicsemi dfu genpkg --dev-type 0x0052 --application $< $@
+
+ifeq ($(BOARD),pinetime)
+all : $(OBJDIR)/reloader-mcuboot.zip
+endif
+
+$(OBJDIR)/reloader-mcuboot.elf: LDSCRIPT = nrf52832_xxaa_mcuboot.ld
+$(OBJDIR)/reloader-mcuboot.elf: $(OBJDIR) $(OBJS) $(LDFILE)
+	$(CROSS_COMPILE)gcc $(LDFLAGS) $(OBJS) -o $@
+
+$(OBJDIR)/reloader-mcuboot.hex: $(OBJDIR)/reloader-mcuboot.elf
+	$(CROSS_COMPILE)objcopy -O ihex $< $@
+
+$(OBJDIR)/reloader-mcuboot.zip : $(OBJDIR)/reloader-mcuboot.hex
 	python3 -m nordicsemi dfu genpkg --dev-type 0x0052 --application $< $@
 
 VPATH = src:lib/nrfx/drivers/src:lib/nrfx/mdk

src/nrf52832_xxaa_mcuboot.ld
@@ -14,8 +14,8 @@ _app_ram_size  = _ram_size - _sd_ram;
 
 MEMORY
 {
-  PREFLASH (rx) : ORIGIN = _app_start, LENGTH = 512
-  FLASH (rx) : ORIGIN = _app_start + 512, LENGTH = _app_size - 512
+  PREFLASH (rx) : ORIGIN = 0x8020, LENGTH = 4K - 0x20
+  FLASH (rx) : ORIGIN = _app_start, LENGTH = _app_size
   RAM (rwx) : ORIGIN = _app_ram_start, LENGTH = _app_ram_size
   /*CODE_RAM (rwx) : ORIGIN = 0x800000, LENGTH = 0x10000*/
 }

src/nrf_common.ld
@@ -1,0 +1,172 @@
+/* Linker script for Nordic Semiconductor nRF devices
+ *
+ * Version: Sourcery G++ 4.5-1
+ * Support: https://support.codesourcery.com/GNUToolchain/
+ *
+ * Copyright (c) 2007, 2008, 2009, 2010 CodeSourcery, Inc.
+ *
+ * The authors hereby grant permission to use, copy, modify, distribute,
+ * and license this software and its documentation for any purpose, provided
+ * that existing copyright notices are retained in all copies and that this
+ * notice is included verbatim in any distributions.  No written agreement,
+ * license, or royalty fee is required for any of the authorized uses.
+ * Modifications to this software may be copyrighted by their authors
+ * and need not follow the licensing terms described here, provided that
+ * the new terms are clearly indicated on the first page of each file where
+ * they apply.
+ */
+OUTPUT_FORMAT ("elf32-littlearm", "elf32-bigarm", "elf32-littlearm")
+
+/* Linker script to place sections and symbol values. Should be used together
+ * with other linker script that defines memory regions FLASH and RAM.
+ * It references following symbols, which must be defined in code:
+ *   Reset_Handler : Entry of reset handler
+ *
+ * It defines following symbols, which code can use without definition:
+ *   __exidx_start
+ *   __exidx_end
+ *   __etext
+ *   __data_start__
+ *   __preinit_array_start
+ *   __preinit_array_end
+ *   __init_array_start
+ *   __init_array_end
+ *   __fini_array_start
+ *   __fini_array_end
+ *   __data_end__
+ *   __bss_start__
+ *   __bss_end__
+ *   __end__
+ *   end
+ *   __HeapBase
+ *   __HeapLimit
+ *   __StackLimit
+ *   __StackTop
+ *   __stack
+ */
+ENTRY(Reset_Handler)
+
+SECTIONS
+{
+    .isr_vector :
+    {
+        KEEP(*(.isr_vector))
+    } > PREFLASH
+
+    .text :
+    {
+        *(.text*)
+
+        KEEP(*(.init))
+        KEEP(*(.fini))
+
+        /* .ctors */
+        *crtbegin.o(.ctors)
+        *crtbegin?.o(.ctors)
+        *(EXCLUDE_FILE(*crtend?.o *crtend.o) .ctors)
+        *(SORT(.ctors.*))
+        *(.ctors)
+
+        /* .dtors */
+        *crtbegin.o(.dtors)
+        *crtbegin?.o(.dtors)
+        *(EXCLUDE_FILE(*crtend?.o *crtend.o) .dtors)
+        *(SORT(.dtors.*))
+        *(.dtors)
+
+        *(.rodata*)
+
+        KEEP(*(.eh_frame*))
+    } > FLASH
+
+    .ARM.extab :
+    {
+        *(.ARM.extab* .gnu.linkonce.armextab.*)
+    } > FLASH
+
+    __exidx_start = .;
+    .ARM.exidx :
+    {
+        *(.ARM.exidx* .gnu.linkonce.armexidx.*)
+    } > FLASH
+    __exidx_end = .;
+
+    __etext = .;
+
+    .data : AT (__etext)
+    {
+        __data_start__ = .;
+        *(vtable)
+        *(.data*)
+
+        . = ALIGN(4);
+        /* preinit data */
+        PROVIDE_HIDDEN (__preinit_array_start = .);
+        KEEP(*(.preinit_array))
+        PROVIDE_HIDDEN (__preinit_array_end = .);
+
+        . = ALIGN(4);
+        /* init data */
+        PROVIDE_HIDDEN (__init_array_start = .);
+        KEEP(*(SORT(.init_array.*)))
+        KEEP(*(.init_array))
+        PROVIDE_HIDDEN (__init_array_end = .);
+
+
+        . = ALIGN(4);
+        /* finit data */
+        PROVIDE_HIDDEN (__fini_array_start = .);
+        KEEP(*(SORT(.fini_array.*)))
+        KEEP(*(.fini_array))
+        PROVIDE_HIDDEN (__fini_array_end = .);
+
+        KEEP(*(.jcr*))
+        . = ALIGN(4);
+        /* All data end */
+        __data_end__ = .;
+
+    } > RAM
+
+    .bss :
+    {
+        . = ALIGN(4);
+        __bss_start__ = .;
+        *(.bss*)
+        *(COMMON)
+        . = ALIGN(4);
+        __bss_end__ = .;
+    } > RAM
+    
+    .heap (COPY):
+    {
+        __HeapBase = .;
+        __end__ = .;
+        PROVIDE(end = .);
+        KEEP(*(.heap*))
+        __HeapLimit = .;
+    } > RAM
+
+    /* .stack_dummy section doesn't contains any symbols. It is only
+     * used for linker to calculate size of stack sections, and assign
+     * values to stack symbols later */
+    .stack_dummy (COPY):
+    {
+        KEEP(*(.stack*))
+    } > RAM
+
+    /* Set stack top to end of RAM, and stack limit move down by
+     * size of stack_dummy section */
+    __StackTop = ORIGIN(RAM) + LENGTH(RAM);
+    __StackLimit = __StackTop - SIZEOF(.stack_dummy);
+    PROVIDE(__stack = __StackTop);
+
+    /* Check if data + heap + stack exceeds RAM limit */
+    ASSERT(__StackLimit >= __HeapLimit, "region RAM overflowed with stack")
+    
+    /* Check if text sections + data exceeds FLASH limit */
+    DataInitFlashUsed = __bss_start__ - __data_start__;
+    CodeFlashUsed = __etext - ORIGIN(FLASH);
+    TotalFlashUsed = CodeFlashUsed + DataInitFlashUsed;
+    ASSERT(TotalFlashUsed <= LENGTH(FLASH), "region FLASH overflowed with .data and user data")
+    
+}

commit 3d960eac16c6cb4ecbe7c42b62d153df84c0f6fb

mcuboot: Integrate autommatic image creation
Fixes: 5bf3b20a248f ("Add mcuboot support")
Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

.gitmodules
@@ -4,3 +4,6 @@
 [submodule "lib/CMSIS"]
 	path = lib/CMSIS
 	url = https://github.com/ARM-software/CMSIS_5/
+[submodule "lib/mcuboot"]
+	path = lib/mcuboot
+	url = https://github.com/mcu-tools/mcuboot

Makefile
@@ -48,10 +48,14 @@ $(OBJDIR)/reloader-mcuboot.elf: LDSCRIPT = nrf52832_xxaa_mcuboot.ld
 $(OBJDIR)/reloader-mcuboot.elf: $(OBJDIR) $(OBJS) $(LDFILE)
 	$(CROSS_COMPILE)gcc $(LDFLAGS) $(OBJS) -o $@
 
-$(OBJDIR)/reloader-mcuboot.hex: $(OBJDIR)/reloader-mcuboot.elf
-	$(CROSS_COMPILE)objcopy -O ihex $< $@
+$(OBJDIR)/reloader-mcuboot.bin: $(OBJDIR)/reloader-mcuboot.elf
+	$(CROSS_COMPILE)objcopy -O binary $< $@.tmp
+	lib/mcuboot/scripts/imgtool.py create \
+		--align 4 --version 1.0.0 \
+		--header-size 32 --slot-size 475136 --pad-header $@.tmp $@
+	$(RM) $@.tmp
 
-$(OBJDIR)/reloader-mcuboot.zip : $(OBJDIR)/reloader-mcuboot.hex
+$(OBJDIR)/reloader-mcuboot.zip : $(OBJDIR)/reloader-mcuboot.bin
 	python3 -m nordicsemi dfu genpkg --dev-type 0x0052 --application $< $@
 
 VPATH = src:lib/nrfx/drivers/src:lib/nrfx/mdk